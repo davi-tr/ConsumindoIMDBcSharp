9fb5e5f41a259696d9edd86d2b204fad85fb1e23
= <=>
2023-05-24 01:54:04 -0300

    Adicionar arquivos de projeto.

---
+++ Imdb.cs
@@ -0,0 +1,90 @@
+﻿namespace QuickType
+{
+
+    public partial class Imdb
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string OriginalTitle { get; set; }
+        public string FullTitle { get; set; }
+        public string Type { get; set; }
+        public long Year { get; set; }
+        public Uri Image { get; set; }
+        public DateTimeOffset ReleaseDate { get; set; }
+        public long RuntimeMins { get; set; }
+        public string RuntimeStr { get; set; }
+        public string Plot { get; set; }
+        public string PlotLocal { get; set; }
+        public bool PlotLocalIsRtl { get; set; }
+        public string Awards { get; set; }
+        public string Directors { get; set; }
+        public CompanyListElement[] DirectorList { get; set; }
+        public string Writers { get; set; }
+        public CompanyListElement[] WriterList { get; set; }
+        public string Stars { get; set; }
+        public CompanyListElement[] StarList { get; set; }
+        public ActorList[] ActorList { get; set; }
+        public object FullCast { get; set; }
+        public string Genres { get; set; }
+        public CountryListElement[] GenreList { get; set; }
+        public string Companies { get; set; }
+        public CompanyListElement[] CompanyList { get; set; }
+        public string Countries { get; set; }
+        public CountryListElement[] CountryList { get; set; }
+        public string Languages { get; set; }
+        public CountryListElement[] LanguageList { get; set; }
+        public string ContentRating { get; set; }
+        public string ImDbRating { get; set; }
+        public long ImDbRatingVotes { get; set; }
+        public long MetacriticRating { get; set; }
+        public object Ratings { get; set; }
+        public object Wikipedia { get; set; }
+        public object Posters { get; set; }
+        public object Images { get; set; }
+        public object Trailer { get; set; }
+        public BoxOffice BoxOffice { get; set; }
+        public object Tagline { get; set; }
+        public string Keywords { get; set; }
+        public string[] KeywordList { get; set; }
+        public Similar[] Similars { get; set; }
+        public object TvSeriesInfo { get; set; }
+        public object TvEpisodeInfo { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+
+    public partial class ActorList
+    {
+        public string Id { get; set; }
+        public Uri Image { get; set; }
+        public string Name { get; set; }
+        public string AsCharacter { get; set; }
+    }
+
+    public partial class BoxOffice
+    {
+        public string Budget { get; set; }
+        public string OpeningWeekendUsa { get; set; }
+        public string GrossUsa { get; set; }
+        public string CumulativeWorldwideGross { get; set; }
+    }
+
+    public partial class CompanyListElement
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public partial class CountryListElement
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+
+    public partial class Similar
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public Uri Image { get; set; }
+        public string ImDbRating { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using QuickType;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsumindoIMDB
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+
+
+            HttpClient client = new HttpClient { BaseAddress = new Uri($"https://imdb-api.com/pt/API/Title/{LoadAPI()}/") };
+            var response = await client.GetAsync("tt4975722");
+            var content = await response.Content.ReadAsStringAsync();
+            string pretty = PrettyJson(content);
+            Console.WriteLine(pretty);
+
+            var filme = JsonConvert.DeserializeObject<Imdb>(content);
+            //Console.WriteLine(filme.Title);
+
+
+            
+        }
+        public static string PrettyJson(string unPrettyJson)
+        {
+            var options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            var jsonElement = System.Text.Json.JsonSerializer.Deserialize<JsonElement>(unPrettyJson);
+
+            return System.Text.Json.JsonSerializer.Serialize(jsonElement, options);
+        }
+
+        public static string LoadAPI()
+        {
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+            string apikey = config["apikey"];
+            return apikey;
+        }
+
+    }
+}
+
